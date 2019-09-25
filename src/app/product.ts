8275d9362db5e56e66eb2971cf7b73ffae8e74a9
suman <30201452+sumanssaurabh@users.noreply.github.com>
2019-09-25 18:11:38 +0530

    build prod

---
+++ src/app/product.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Phone X',
+    price: 899,
+    description: 'A great phone with touch body'
+  }
+];
