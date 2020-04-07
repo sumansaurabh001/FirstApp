6487fc0352b28f4bcef44f9e10618c9931043302
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-07 11:10:03 +0000

    Deploy sumansaurabh001/FirstApp to github.com/sumansaurabh001/FirstApp.git:gh-pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { InspireComponent } from './inspire/inspire.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    TopBarComponent,
+    InspireComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'inspire', component: InspireComponent },
+      { path: 'shipping', component: ShippingComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/inspire/inspire.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InspireComponent } from './inspire.component';
+
+describe('InspireComponent', () => {
+  let component: InspireComponent;
+  let fixture: ComponentFixture<InspireComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InspireComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InspireComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
